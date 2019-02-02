4490cf88a1fd8cb554bb37da46bdca8a5b3d90c3
Yoeza Sapoetro <yoezasapoetro@gmail.com>
2019-02-03 03:13:15 +0700

    [change] simplify code for command

--- scales.js
+++ scales.js
@@ -5,10 +5,5 @@ const program = require('commander');
 
 program
     .version('0.0.2')
-    .option('-s, --start-stream', 'Start Stream', () => {
-        serial((data) => {
-            let serialData = data.match(/[(0-9)(.)]/g);
-            console.log(serialData.join(""))
-        });
-    })
+    .option('-s, --start-stream', 'Start Stream', () => serial(console.log))
     .parse(process.argv);
